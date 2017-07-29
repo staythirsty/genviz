e05bcdf30a86b625d3bbde8974888c46329dd67a
staythirsty <navn@Naveens-MacBook-Pro.local>
2017-07-29 09:51:08 -0400

    port evn

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require('express')
 var vizualizer = require('./routers/vizualizer')
 
 var app = express();
+var port = process.env.PORT || 5000;
+
 
 app.use(express.static('public'))
 
 app.use('/viz', vizualizer)
 
-app.listen(5000, function () {
+app.listen(port, function () {
   console.log('listening on port 5000!')
 })
